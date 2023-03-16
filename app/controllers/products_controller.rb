033be4929ecc4b6fafba518747537e1b32385a66
Andres Munoz <andremude@hotmail.com>
2023-03-16 17:07:33 -0500

    add create product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,17 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: "Product was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
