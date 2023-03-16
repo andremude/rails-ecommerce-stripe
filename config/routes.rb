5a48c9b134f08a1ffbbddaeea59d3ff13f794667
Andres Munoz <andremude@hotmail.com>
2023-03-16 17:35:55 -0500

    products

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :articles, except: [:destroy]
+  resources :products, except: [:destroy]
   resources :checkout, only: [:create]
   resources :webhooks, only: [:create]
 end
