03ec9a25a4cd6f31acf8637e0a3cf891f44b9410
Andres Munoz <andremude@hotmail.com>
2023-03-17 22:22:32 -0500

    add create

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,18 +1,25 @@
 class CheckoutController < ApplicationController
 
   def create
+    product = Product.find(params[:id])
     session = Stripe::Checkout::Session.create({
       payment_method_types: ['card'],
       line_items: [{
-        name: product.name,
-        amount: product.price,
-        currency: "usd",
+        price_data: {
+          currency: "usd",
+          unit_amount: product.price,
+            product_data: {
+              name: product.name
+            }
+        },
         quantity: 1
       }],
       mode: 'payment',
       success_url: root_url,
       cancel_url: root_url
     })
+    respond_to do |format|
+      format.js
+    end
   end
-
 end
