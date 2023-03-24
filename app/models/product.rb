6501e1ece003e29f04f66b66dc4ff9788cfeeaa8
Andres Munoz <andremude@hotmail.com>
2023-03-23 21:11:43 -0500

    add stripe product id to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
   validates :name, :price, presence: true
+
+  def to_s
+    name
+  end
+
+  after_create do
+    product = Stripe::Product.create(name: name)
+    price = Stripe::Price.create(product: product, unit_amount: self.price, currency: "usd" )
+    update(stripe_product_id: product.id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_22_025848) do
+ActiveRecord::Schema.define(version: 2023_03_24_015425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2023_03_22_025848) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "sales_count", default: 0, null: false
+    t.string "stripe_product_id"
   end
 
   create_table "users", force: :cascade do |t|
