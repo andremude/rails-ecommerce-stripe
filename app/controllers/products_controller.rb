25cb188346dfe836d7877db22fc7a3d11874bbf6
Andres Munoz <andremude@hotmail.com>
2023-03-26 20:16:06 -0500

    add session based cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,6 +35,18 @@ class ProductsController < ApplicationController
     redirect_to root_url, notice: "Product was successfully destroyed."
   end
 
+  def add_to_cart
+    id = params[:id].to_i
+    session[:cart] << id unless session[:cart].include?(id)
+    redirect_to products_path
+  end
+
+  def remove_from_cart
+    id = params[:id].to_i
+    session[:cart].delete(id)
+    redirect_to products_path
+  end
+
   private
 
   def product_params
